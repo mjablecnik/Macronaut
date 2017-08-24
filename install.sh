024a2ed727b724570e4a01827bed524ddfd1aa6c
Martin Jablečník <martin.jablecnik@email.cz>
2017-08-24 23:09:05 +0200

    moved macronaut main script into src directory

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cp -r lib/* /usr/lib/python2.7/
-cp -r src/macro-creator.py /usr/bin/macro-creator
-cp Macronaut /usr/bin/macronaut
+cp src/macro-creator.py /usr/bin/macro-creator
+cp src/main.sh /usr/bin/macronaut
 
 
