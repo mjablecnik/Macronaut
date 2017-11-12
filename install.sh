2c44b5e078ea78c6749846d7c86897442a6c2054
Martin Jablečník <martin.jablecnik@email.cz>
2017-11-12 23:25:23 +0100

    replaced perl command to sed

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ set -e
 pip install pynput==1.3.7 
 apt-get install xdotool
 
-echo src/config.py | xargs perl -pi -E "s/DEVEL=True/DEVEL=False/g"
+sed -i -e 's/DEVEL=True/DEVEL=False/g' src/config.py
 
 cp -r src/*.py /usr/lib/python2.7/
 cp src/macronaut /usr/bin/macronaut
