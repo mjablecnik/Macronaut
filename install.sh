6a720b26f8c5667149aa9f4a9e8d855f924c3a83
Martin Jablečník <martin.jablecnik@email.cz>
2017-12-02 20:49:07 +0100

    fix installation

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-pip install pynput==1.3.7 docopt=0.6.2
+pip install pynput==1.3.7 docopt==0.6.2
 apt-get install xdotool
 
 sed -i -e 's/DEVEL=True/DEVEL=False/g' src/config.py
